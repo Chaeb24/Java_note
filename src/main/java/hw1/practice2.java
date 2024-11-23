d080300070594f55700d0519e1aee9ca87e4ff22
Chaeb24 <jisusushi@gmail.com>
2024-11-23 15:52:10 +0900

    스터디 실습문제 수정

--- src/main/java/hw1/practice2.java
+++ src/main/java/hw1/practice2.java
@@ -10,6 +10,15 @@ public class practice2 {
 
         System.out.println(str);
 
+        char[] charArray = sentence.toCharArray();
+        //문자열을 char형으로 변환하는 공식
+
+        StringBuilder reversed = new StringBuilder();
+
+        for(int i=charArray.length-1;i>=0;i--) //문자열 4부터 시작
+            reversed.append(charArray[i]);
+
+        System.out.println(reversed);
 
     }
 }
